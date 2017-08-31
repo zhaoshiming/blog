575d1d2b3ef807844d62b96d96e43b13c2452600
zhaoshiming <1406554044@qq.com>
2017-08-31 18:36:32 +0800

    asdasd

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 window.onload = function  () {
 	console.log('欢迎欢迎！');
 	var headImg = document.getElementsByClassName('head');
-	headImg.style.backgroundImage = "url('/img/head1.jpg')";
+	headImg.style.backgroundImage = "url(http://www.siamess.cn/img/head2.jpg)";
 }
